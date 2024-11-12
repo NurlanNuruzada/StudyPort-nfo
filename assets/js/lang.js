844308bd920f7fbaa5a2eebd96260d4b2b3a264a
nurlannuruzada <nurlangn@code.edu.az>
2024-11-12 04:15:41 +0400

    feat: done ✨

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,222 +1,8 @@
+import en from './Translate/en.js';
+import ru from './Translate/ru.js';
+import az from './Translate/az.js';
 
-const translations = {
-    en: {
-        home: "Home",
-        about: "About",
-        statistic: "Statistic",
-        pricing: "Pricing",
-        faq: "FAQ",
-        Contact:"Contact",
-        "hero-title": "Open-Source Web Template for SaaS, Startup, Apps, and More",
-        "hero-desc": "Multidisciplinary Web Template Built with Your Favourite Technology - HTML Bootstrap, Tailwind and React NextJS.",
-        "features-section-title": "Features",
-        "features-main-title": "Main Features of Play",
-        "features-desc": "There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form.",
-        "feature-1-title": "Free and Open-Source",
-        "feature-1-desc": "Lorem Ipsum is simply dummy text of the printing and industry.",
-        "feature-2-title": "Multipurpose Template",
-        "feature-2-desc": "Lorem Ipsum is simply dummy text of the printing and industry.",
-        "feature-3-title": "High-quality Design",
-        "feature-3-desc": "Lorem Ipsum is simply dummy text of the printing and industry.",
-        "feature-4-title": "All Essential Elements",
-        "feature-4-desc": "Lorem Ipsum is simply dummy text of the printing and industry.",
-        "feature-learn-more": "Learn More",
-        "about-us-tag": "About Us",
-        "about-main-title": "Brilliant Toolkit to Build Nextgen Website Faster.",
-        "about-desc-1": "The main ‘thrust’ is to focus on educating attendees on how to best protect highly vulnerable business applications with interactive panel discussions and roundtables led by subject matter experts.",
-        "about-desc-2": "The main ‘thrust’ is to focus on educating attendees on how to best protect highly vulnerable business applications with interactive panel.",
-        "about-learn-more": "Learn More",
-        "statistics-title": "Statistics",
-        "statistics-heading": "Our Statistics",
-        "statistics-desc": "There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form.",
-        "stats-some": "Some statistics",
-        "slider": "slider",
-        "slider-title": "Our Slider",
-        "slider-desc": "There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form.",
-        "slider-first-slide-label-1": "First slide label 1",
-        "slider-first-slide-label-2": "First slide label 2",
-        "slider-text-desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim suscipit quasi corporis labore quas dolore quam incidunt obcaecati ducimus, dolores dolor quis culpa? Neque nisi quibusdam alias provident qui numquam.",
-        "pricing": "Pricing",
-        "ourPricingPlans": "Our Pricing Plans",
-        "pricingDescription": "There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form.",
-        "firstButton": "First",
-        "secondButton": "Second",
-        "startingFrom": "STARTING FROM",
-        "price": "$ 19.99/mo",
-        "pricingItem1": "5 Users",
-        "pricingItem2": "All UI components",
-        "pricingItem3": "Lifetime access",
-        "pricingItem4": "Free updates",
-        "pricingItem5": "Use on 1 (one) project",
-        "pricingItem6": "4 Months support",
-        "purchaseNow": "Purchase Now",
-        "startingFrom2": "STARTING FROM 2",
-        "faq": "FAQ",
-        "faqTitle": "Any Questions? Answered",
-        "faqDescription": "There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form.",
-        "question1": "How to use UIdeck?",
-        "answer1": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        "question2": "How to download icons from Lineicons?",
-        "answer2": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        "question3": "Is GrayGrids part of UIdeck?",
-        "answer3": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        "question4": "Can I use this template for commercial project?",
-        "answer4": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        "question5": "Do you have plan releasing Play Pro?",
-        "answer5": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        "question6": "Where and how to host this template?",
-        "answer6": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        "contact-title": "CONTACT US",
-        "contact-subtitle": "Let’s talk about .Love to hear from you!",
-        "contact-emailTitle": "How Can We Help?",
-        "contact-callTitle": "You can call us"
-    },
-    ru: {
-        home: "Главная",
-        about: "О нас",
-        statistic: "Статистика",
-        pricing: "Цены",
-        Contact: "Контакт",
-        faq: "Часто задаваемые вопросы",
-        "hero-title": "Открытый веб-шаблон для SaaS, стартапов, приложений и многого другого",
-        "hero-desc": "Многопрофильный веб-шаблон, созданный с использованием ваших любимых технологий - HTML Bootstrap, Tailwind и React NextJS.",
-        "features-section-title": "Особенности",
-        "features-main-title": "Основные особенности Play",
-        "features-desc": "Существует множество вариантов отрывков Lorem Ipsum, доступных, но большинство из них были изменены в той или иной форме.",
-        "feature-1-title": "Бесплатно и с открытым исходным кодом",
-        "feature-1-desc": "Lorem Ipsum - это просто фиктивный текст в типографике и издательской отрасли.",
-        "feature-2-title": "Многоцелевой шаблон",
-        "feature-2-desc": "Lorem Ipsum - это просто фиктивный текст в типографике и издательской отрасли.",
-        "feature-3-title": "Высококачественный дизайн",
-        "feature-3-desc": "Lorem Ipsum - это просто фиктивный текст в типографике и издательской отрасли.",
-        "feature-4-title": "Все основные элементы",
-        "feature-4-desc": "Lorem Ipsum - это просто фиктивный текст в типографике и издательской отрасли.",
-        "feature-learn-more": "Узнать больше",
-        "about-us-tag": "О нас",
-        "about-main-title": "Блестящий инструмент для более быстрого создания сайтов следующего поколения.",
-        "about-desc-1": "Основной целью является обучение участников, как лучше всего защищать высоко уязвимые бизнес-приложения с помощью интерактивных панельных дискуссий и круглых столов, проводимых экспертами.",
-        "about-desc-2": "Основной целью является обучение участников, как лучше всего защищать высоко уязвимые бизнес-приложения с помощью интерактивных панелей.",
-        "about-learn-more": "Узнать больше",
-        "statistics-title": "Статистика",
-        "statistics-heading": "Наша статистика",
-        "statistics-desc": "Существует множество вариантов отрывков Lorem Ipsum, доступных, но большинство из них были изменены в той или иной форме.",
-        "stats-some": "Некоторые статистические данные",
-        "slider": "слайда",
-        "slider-title": "Наш слайдер",
-        "slider-desc": "Существует множество вариантов отрывков Lorem Ipsum, доступных, но большинство из них были изменены в той или иной форме.",
-        "slider-first-slide-label-1": "Метка первого слайда 1",
-        "slider-first-slide-label-2": "Метка первого слайда 2",
-        "slider-text-desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim suscipit quasi corporis labore quas dolore quam incidunt obcaecati ducimus, dolores dolor quis culpa? Neque nisi quibusdam alias provident qui numquam.",
-        "pricing": "Цены",
-        "ourPricingPlans": "Наши тарифные планы",
-        "pricingDescription": "Существует множество вариантов Lorem Ipsum, но большинство из них были изменены.",
-        "firstButton": "Первый",
-        "secondButton": "Второй",
-        "startingFrom": "НАЧИНАЕТСЯ С",
-        "price": "$ 19.99/мес",
-        "pricingItem1": "5 пользователей",
-        "pricingItem2": "Все компоненты UI",
-        "pricingItem3": "Пожизненный доступ",
-        "pricingItem4": "Бесплатные обновления",
-        "pricingItem5": "Использование на 1 (одном) проекте",
-        "pricingItem6": "4 месяца поддержки",
-        "purchaseNow": "Купить сейчас",
-        "startingFrom2": "НАЧИНАЕТСЯ С 2",
-        "faq": "Часто задаваемые вопросы",
-        "faqTitle": "Есть вопросы? Ответы на них",
-        "faqDescription": "Существует множество вариантов Lorem Ipsum, но большинство из них были изменены.",
-        "question1": "Как использовать UIdeck?",
-        "answer1": "Lorem Ipsum — это просто фиктивный текст в полиграфической и наборной промышленности. Lorem Ipsum был стандартом текста с 1500-х годов, когда неизвестный типограф взял набор букв и перемешал их, чтобы создать образец шрифта.",
-        "question2": "Как скачать иконки с Lineicons?",
-        "answer2": "Lorem Ipsum — это просто фиктивный текст в полиграфической и наборной промышленности. Lorem Ipsum был стандартом текста с 1500-х годов, когда неизвестный типограф взял набор букв и перемешал их, чтобы создать образец шрифта.",
-        "question3": "Является ли GrayGrids частью UIdeck?",
-        "answer3": "Lorem Ipsum — это просто фиктивный текст в полиграфической и наборной промышленности. Lorem Ipsum был стандартом текста с 1500-х годов, когда неизвестный типограф взял набор букв и перемешал их, чтобы создать образец шрифта.",
-        "question4": "Могу ли я использовать этот шаблон для коммерческого проекта?",
-        "answer4": "Lorem Ipsum — это просто фиктивный текст в полиграфической и наборной промышленности. Lorem Ipsum был стандартом текста с 1500-х годов, когда неизвестный типограф взял набор букв и перемешал их, чтобы создать образец шрифта.",
-        "question5": "Планируется ли выпуск Play Pro?",
-        "answer5": "Lorem Ipsum — это просто фиктивный текст в полиграфической и наборной промышленности. Lorem Ipsum был стандартом текста с 1500-х годов, когда неизвестный типограф взял набор букв и перемешал их, чтобы создать образец шрифта.",
-        "question6": "Где и как разместить этот шаблон?",
-        "answer6": "Lorem Ipsum — это просто фиктивный текст в полиграфической и наборной промышленности. Lorem Ipsum был стандартом текста с 1500-х годов, когда неизвестный типограф взял набор букв и перемешал их, чтобы создать образец шрифта.",
-        "contact-title": "СВЯЖИТЕСЬ С НАМИ",
-        "contact-subtitle": "Давайте поговорим .Будем рады услышать от вас!",
-        "contact-emailTitle": "Как мы можем помочь?",
-        "contact-callTitle": "Вы можете позвонить нам",
-    },
-    az: {
-        home: "Ev",
-        about: "Haqqında",
-        statistic: "Statistika",
-        pricing: "Qiymətlər",
-        faq: "Tez-tez verilən suallar",
-        Contact:"Əlaqə",
-        "hero-title": "SaaS, Startap, Tətbiqlər və Daha Çoxu üçün Açıq Mənbə Web Şablonu",
-        "hero-desc": "Sevən Texnologiyalarınızla Qurulmuş Multidisiplinar Web Şablonu - HTML Bootstrap, Tailwind və React NextJS.",
-        "features-section-title": "Xüsusiyyətlər",
-        "features-main-title": "Play-in Əsas Xüsusiyyətləri",
-        "features-desc": "Lorem Ipsum-un bir çox variantı mövcuddur, amma əksəriyyəti müəyyən bir formada dəyişikliklərə uğramışdır.",
-        "feature-1-title": "Pulsuz və Açıq Mənbə",
-        "feature-1-desc": "Lorem Ipsum sadəcə çap və sənaye üçün istifadə olunan yalançı mətndir.",
-        "feature-2-title": "Çoxməqsədli Şablon",
-        "feature-2-desc": "Lorem Ipsum sadəcə çap və sənaye üçün istifadə olunan yalançı mətndir.",
-        "feature-3-title": "Yüksək Keyfiyyətli Dizayn",
-        "feature-3-desc": "Lorem Ipsum sadəcə çap və sənaye üçün istifadə olunan yalançı mətndir.",
-        "feature-4-title": "Bütün Əsas Elementlər",
-        "feature-4-desc": "Lorem Ipsum sadəcə çap və sənaye üçün istifadə olunan yalançı mətndir.",
-        "feature-learn-more": "Daha çox öyrən",
-        "about-us-tag": "Haqqımızda",
-        "about-main-title": "Növbəti nəsil vebsaytları daha sürətli qurmaq üçün möhtəşəm alət.",
-        "about-desc-1": "Əsas məqsəd, iştirakçılara yüksək dərəcədə zəif biznes tətbiqlərini necə qorumaq barədə təlim keçməkdir, interaktiv panel müzakirələri və mövzu üzrə mütəxəssislər tərəfindən aparılan yuvarlaq masa müzakirələri ilə.",
-        "about-desc-2": "Əsas məqsəd, iştirakçılara yüksək dərəcədə zəif biznes tətbiqlərini necə qorumaq barədə təlim keçməkdir, interaktiv panel müzakirələri ilə.",
-        "about-learn-more": "Daha çox öyrən",
-        "statistics-title": "Statistika",
-        "statistics-heading": "Bizim Statistikanın",
-        "statistics-desc": "Lorem Ipsum-nun bir çox variantı mövcuddur, amma əksəriyyəti müəyyən bir formada dəyişikliklərə uğramışdır.",
-        "slider": "slider",
-        "stats-some": "Bəzi statistikalar",
-        "slider-title": "Bizim Səyyah",
-        "slider-desc": "Lorem Ipsum-un bir çox variantı mövcuddur, amma əksəriyyəti müəyyən bir formada dəyişikliklərə uğramışdır.",
-        "slider-first-slide-label-1": "Birinci slayd etiket 1",
-        "slider-first-slide-label-2": "Birinci slayd etiket 2",
-        "slider-text-desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim suscipit quasi corporis labore quas dolore quam incidunt obcaecati ducimus, dolores dolor quis culpa? Neque nisi quibusdam alias provident qui numquam.",
-        "pricing": "Qiymətləndirmə",
-        "ourPricingPlans": "Bizim Qiymət Planlarımız",
-        "pricingDescription": "Lorem Ipsum-un bir çox variantları mövcuddur, lakin onların əksəriyyəti bəzi dəyişikliklərə uğramışdır.",
-        "firstButton": "Birinci",
-        "secondButton": "İkinci",
-        "startingFrom": "BAŞLANIR",
-        "price": "$ 19.99/ay",
-        "pricingItem1": "5 İstifadəçi",
-        "pricingItem2": "Bütün UI komponentləri",
-        "pricingItem3": "Həyat boyu giriş",
-        "pricingItem4": "Pulsuz yeniliklər",
-        "pricingItem5": "1 (bir) layihədə istifadə",
-        "pricingItem6": "4 Ay dəstək",
-        "purchaseNow": "İndi Al",
-        "startingFrom2": "BAŞLANIR 2",
-        "faq": "Tez-tez verilən suallar",
-        "faqTitle": "Hər hansı sualınız varmı? Cavablar burada",
-        "faqDescription": "Lorem Ipsum-un bir çox variantları mövcuddur, lakin onların əksəriyyəti bəzi dəyişikliklərə uğramışdır.",
-        "question1": "UIdeck necə istifadə olunur?",
-        "answer1": "Lorem Ipsum sadəcə çap və tərtibat sənayesinin nümunə mətnidir. Lorem Ipsum bu sənayenin standart mətnidir 1500-cü illərdən bəri, bilinməyən bir çapçı, hərfləri qarışdıraraq bir tip nümunəsi kitabı hazırlamışdır.",
-        "question2": "Lineicons-dan ikonları necə yükləmək olar?",
-        "answer2": "Lorem Ipsum sadəcə çap və tərtibat sənayesinin nümunə mətnidir. Lorem Ipsum bu sənayenin standart mətnidir 1500-cü illərdən bəri, bilinməyən bir çapçı, hərfləri qarışdıraraq bir tip nümunəsi kitabı hazırlamışdır.",
-        "question3": "GrayGrids UIdeck-in bir hissəsidirmi?",
-        "answer3": "Lorem Ipsum sadəcə çap və tərtibat sənayesinin nümunə mətnidir. Lorem Ipsum bu sənayenin standart mətnidir 1500-cü illərdən bəri, bilinməyən bir çapçı, hərfləri qarışdıraraq bir tip nümunəsi kitabı hazırlamışdır.",
-        "question4": "Bu şablonu kommersiya layihəsində istifadə edə bilərəmmi?",
-        "answer4": "Lorem Ipsum sadəcə çap və tərtibat sənayesinin nümunə mətnidir. Lorem Ipsum bu sənayenin standart mətnidir 1500-cü illərdən bəri, bilinməyən bir çapçı, hərfləri qarışdıraraq bir tip nümunəsi kitabı hazırlamışdır.",
-        "question5": "Play Pro versiyasını buraxmaq planınız varmı?",
-        "answer5": "Lorem Ipsum sadəcə çap və tərtibat sənayesinin nümunə mətnidir. Lorem Ipsum bu sənayenin standart mətnidir 1500-cü illərdən bəri, bilinməyən bir çapçı, hərfləri qarışdıraraq bir tip nümunəsi kitabı hazırlamışdır.",
-        "question6": "Bu şablonu harada və necə yerləşdirmək olar?",
-        "answer6": "Lorem Ipsum sadəcə çap və tərtibat sənayesinin nümunə mətnidir. Lorem Ipsum bu sənayenin standart mətnidir 1500-cü illərdən bəri, bilinməyən bir çapçı, hərfləri qarışdıraraq bir tip nümunəsi kitabı hazırlamışdır.",
-        "contact-title": "BİZİ ƏLAQƏ SAXLAYIN",
-        "contact-subtitle": "Gəlin danışaq. Sizdən eşitmək istərdik!",
-        "contact-emailTitle": "Necə kömək edə bilərik?",
-        "contact-callTitle": "Bizə zəng edə bilərsiniz",
-    },
-};
-
-
-
+const translations = { en, ru, az };
 
 // Elements
 const dropdownButton = document.getElementById('LanguageButton');
@@ -225,14 +11,14 @@ const arrowIcon = dropdownButton.querySelector('svg');
 const buttonText = document.getElementById('lang-text');
 
 // Open and close the dropdown menu on button click
-dropdownButton.addEventListener('click', function (event) {
+dropdownButton.addEventListener('click', (event) => {
     event.stopPropagation();
     dropdownMenu.classList.toggle('show');
     arrowIcon.classList.toggle('arrow-down');
 });
 
 // Close the dropdown if clicking outside
-window.addEventListener('click', function (event) {
+window.addEventListener('click', (event) => {
     if (!event.target.closest('#LanguageButton')) {
         dropdownMenu.classList.remove('show');
         arrowIcon.classList.remove('arrow-down');
@@ -241,7 +27,7 @@ window.addEventListener('click', function (event) {
 
 // Translate elements based on selected language
 function applyTranslations(lang) {
-    document.querySelectorAll('[data-i18n-key]').forEach(element => {
+    document.querySelectorAll('[data-i18n-key]').forEach((element) => {
         const key = element.getAttribute('data-i18n-key');
         if (translations[lang] && translations[lang][key]) {
             element.innerText = translations[lang][key];
@@ -250,17 +36,16 @@ function applyTranslations(lang) {
 }
 
 // Change language and update UI
-function changeLanguage(lang) {
-    // Update button label to show selected language
+window.changeLanguage = function (lang) {
     buttonText.innerHTML = lang === 'en' ? 'En' : lang === 'ru' ? 'Ru' : 'Az';
     dropdownMenu.classList.remove('show');
     arrowIcon.classList.remove('arrow-down');
 
     // Apply the translations
     applyTranslations(lang);
-}
+};
 
-// Initialize page with default language (e.g., English)
+// Initialize page with default language
 document.addEventListener("DOMContentLoaded", () => {
-    changeLanguage('en'); // Load default language (can be 'en' or 'ru' or 'az')
+    changeLanguage('en'); // Default language
 });
